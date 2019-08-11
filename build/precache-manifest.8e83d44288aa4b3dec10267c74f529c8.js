1af928b85bd036112724806fe01161c780f8c945
admin <admin@gov.com>
2019-08-11 15:13:47 +0800

    基本骨架搭建完成

---
+++ build/precache-manifest.8e83d44288aa4b3dec10267c74f529c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fb7373e2554e282b59a45b230791ad2",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "90c00921a207c2eda54e",
+    "url": "/TodoList/static/css/2.16b94f83.chunk.css"
+  },
+  {
+    "revision": "00262039d7c5bf9a0a47",
+    "url": "/TodoList/static/css/main.eab24d3c.chunk.css"
+  },
+  {
+    "revision": "90c00921a207c2eda54e",
+    "url": "/TodoList/static/js/2.aa1b5c66.chunk.js"
+  },
+  {
+    "revision": "00262039d7c5bf9a0a47",
+    "url": "/TodoList/static/js/main.d0c48043.chunk.js"
+  },
+  {
+    "revision": "2941d59632408adad134",
+    "url": "/TodoList/static/js/runtime~main.9664ef8a.js"
+  }
+]);
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
-import { Button } from 'antd';
 import './App.css';
 
-class App extends Component {
+interface IAppState {
+  newTodo: string,
+  todoList: [{ id: number, title: string }]
+}
+
+class App extends Component<any, IAppState> {
+  constructor(props: any) {
+    super(props)
+    this.state = {
+      newTodo: 'test',
+      todoList: [
+        { id: 1, title: '第一个待办' }
+      ]
+    }
+  }
   render() {
+    let todos = this.state.todoList.map((item, index) => {
+      return <li>{item.title}</li>
+    })
     return (
       <div className="App">
-        <Button type="primary">Button</Button>
+        <h1>我的待办</h1>
+        <div className="inputWrapper">
+          <input type="text" value={this.state.newTodo} />
+        </div>
+        <ol>
+          {todos}
+        </ol>
       </div>
-    );
+    )
   }
 }
+
+
 
 export default App;
