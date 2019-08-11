557833b53a7093e3d002f13d3a59d1b6815f5e0c
QIMengz <623848922@qq.com>
2019-08-11 17:07:29 +0800

    添加回车输入 Todo ，并且清空

--- src/App.tsx
+++ src/App.tsx
@@ -5,35 +5,39 @@ import 'normalize.css'
 import './reset.css'
 import './App.css'
 
+
 interface IAppState {
   newTodo: string,
-  todoList: [{ id: number, title: string }, { id: number, title: string }]
+  todoList: any[]
 }
 
 class App extends Component<any, IAppState> {
   constructor(props: any) {
     super(props)
     this.state = {
-      newTodo: 'test',
-      todoList: [
-        { id: 1, title: '第一个待办' },
-        { id: 2, title: '第二个待办' }
-      ]
+      newTodo: '',
+      todoList: []
     }
   }
   render() {
     let todos = this.state.todoList.map((item, index) => {
       return (
-        <li>
+        <li key={index}>
           <TodoItem todo={item} />
         </li>
       )
     })
+
+
     return (
       <div className="App">
         <h1>我的待办</h1>
         <div className="inputWrapper">
-          <TodoInput content={this.state.newTodo} />
+          <TodoInput
+            content={this.state.newTodo}
+            onChange={this.changeTitle.bind(this)}
+            onSubmit={this.addTodo.bind(this)}
+          />
         </div>
         <ol>
           {todos}
@@ -41,6 +45,32 @@ class App extends Component<any, IAppState> {
       </div>
     )
   }
+
+  changeTitle(event: any) {
+    this.setState({
+      newTodo: event.target.value,
+      todoList: this.state.todoList
+    })
+  }
+
+  addTodo(event: any) {
+    this.state.todoList.push({
+      id: idMaker(),
+      title: event.target.value,
+      status: null,
+      deleted: false
+    })
+    this.setState({
+      newTodo: '',
+      todoList: this.state.todoList
+    })
+  }
+}
+
+let id = 0
+function idMaker() {
+  id += 1
+  return id
 }
 
 
--- src/components/TodoInput.tsx
+++ src/components/TodoInput.tsx
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 
 interface ITodoInputProps {
-  content: string
+  content: string,
+  onSubmit: (event: any) => void,
+  onChange: (event: any) => void
 }
 
 export default class TodoInput extends Component<ITodoInputProps> {
   render() {
-    return <input type="text" value={this.props.content} />
+    return <input type="text"
+      value={this.props.content}
+      onChange={this.changeTitle.bind(this)}
+      onKeyPress={this.submit.bind(this)} />
+
+  }
+
+  changeTitle(e: any) {
+    this.props.onChange(e)
+  }
+
+  submit(e: any) {
+    if (e.key === 'Enter') {
+      this.props.onSubmit(e)
+    }
   }
 }
