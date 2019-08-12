48b82de6a0f61cb87caec4b8979f722da3735add
admin <admin@gov.com>
2019-08-12 16:35:49 +0800

    添加 leanCloud

---
+++ src/leanCloud.tsx
@@ -0,0 +1,11 @@
+import AV from 'leancloud-storage'
+
+var APP_ID = 'Cm3g0e6jj5b91eJ8rUVuubGb-MdYXbMMI';
+var APP_KEY = 'Nq82JXJqI3dQw44wjULW12el';
+
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+})
+
+export default AV
