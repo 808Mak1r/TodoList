8ec6554bee29f0434d487eedd63f926ecbbad70f
admin <admin@gov.com>
2019-08-20 09:19:47 +0800

    发布预览

--- build/precache-manifest.c8e8cca2813bfe9a17fd9af6caf65ae2.js
+++ build/precache-manifest.c8e8cca2813bfe9a17fd9af6caf65ae2.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4503d9c58468b98822752a19fd04d645",
+    "revision": "d2fde0112c270dbeab352433d0faccc8",
     "url": "/TodoList/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/TodoList/static/css/2.764ccc25.chunk.css"
   },
   {
-    "revision": "b181bc62a2f9c23ee05f",
+    "revision": "dab71a31ada1d2e9a551",
     "url": "/TodoList/static/css/main.ab8c86c1.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/TodoList/static/js/2.469f4afd.chunk.js"
   },
   {
-    "revision": "b181bc62a2f9c23ee05f",
-    "url": "/TodoList/static/js/main.6679e601.chunk.js"
+    "revision": "dab71a31ada1d2e9a551",
+    "url": "/TodoList/static/js/main.5dc70a25.chunk.js"
   },
   {
     "revision": "2941d59632408adad134",
