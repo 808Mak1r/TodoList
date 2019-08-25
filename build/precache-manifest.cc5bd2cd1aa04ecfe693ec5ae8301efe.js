135445c5864b4614fe0bfd2dbcb5ab1a166f4935
admin <admin@gov.com>
2019-08-25 13:13:59 +0800

    样式更新

---
+++ build/precache-manifest.cc5bd2cd1aa04ecfe693ec5ae8301efe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4abce0b65a0c68011664a4393ca62fa9",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "0a3b44b43d0ef2ca4914",
+    "url": "/TodoList/static/css/2.e9ff3914.chunk.css"
+  },
+  {
+    "revision": "577fb8e86ecdfe55753f",
+    "url": "/TodoList/static/css/main.6efe7ee7.chunk.css"
+  },
+  {
+    "revision": "0a3b44b43d0ef2ca4914",
+    "url": "/TodoList/static/js/2.b665ef3a.chunk.js"
+  },
+  {
+    "revision": "577fb8e86ecdfe55753f",
+    "url": "/TodoList/static/js/main.8687d378.chunk.js"
+  },
+  {
+    "revision": "2941d59632408adad134",
+    "url": "/TodoList/static/js/runtime~main.9664ef8a.js"
+  }
+]);
--- src/components/SignInForm/SignInForm.jsx
+++ src/components/SignInForm/SignInForm.jsx
@@ -1,21 +1,22 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
+import { Input, Button } from 'antd'
 
 export default function (props) {
   return (
     <form className="signIn" onSubmit={props.onSubmit}> {/* 登录*/}
       <div className="row">
         <label>用户名</label>
-        <input type="text" value={props.formData.username}
+        <Input type="text" value={props.formData.username}
           onChange={props.onChange.bind(null, 'username')} />
       </div>
       <div className="row">
         <label>密码</label>
-        <input type="password" value={props.formData.password}
+        <Input type="password" value={props.formData.password}
           onChange={props.onChange.bind(null, 'password')} />
       </div>
       <div className="row actions">
-        <button type="submit">登录</button>
+        <Button htmlType="submit">登录</Button>
         <a href="#" onClick={props.onForgotPassword}>忘记密码了？</a>
       </div>
     </form>
